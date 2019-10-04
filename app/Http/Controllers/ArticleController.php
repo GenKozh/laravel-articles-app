bf18477c62c9eb229926d5c95953aa00735dd317
GenKozh <gen.kozh@gmail.com>
2019-10-04 22:56:47 +0300

    Simple Laravel CRUD application, first commit, index view, article create view, database

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Article;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = Article::all();
+
+        return view('index', compact('articles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'author_name'=>'required',
+            'title'=>'required',
+            'article_text'=>'required'
+        ]);
+
+        $article = new Article([
+            'author_name' => $request->get('author_name'),
+            'title' => $request->get('title'),
+            'article_text' => $request->get('article_text'),
+        ]);
+        $article->save();
+        return redirect('/articles')->with('success', 'Artircle is saved!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $article = Article::find($id);
+        return view('articles.edit', compact('article'));      
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'author_name'=>'required',
+            'title'=>'required',
+            'article_text'=>'required'
+        ]);
+
+        $article = Article::find($id);
+        $article->author_name =  $request->get('author_name');
+        $article->title = $request->get('title');
+        $article->article_text = $request->get('article_text');
+        $article->save();
+
+        return redirect('/articles')->with('success', 'Article updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
