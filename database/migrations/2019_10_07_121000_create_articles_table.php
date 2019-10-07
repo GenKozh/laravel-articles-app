068a2fb9fcd401edc5dee06e0cb92be72e059539
GenKozh <gen.kozh@gmail.com>
2019-10-07 15:11:08 +0300

    database migration added

---
+++ database/migrations/2019_10_07_121000_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateArticlesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('articles', function(Blueprint $table)
+		{
+			$table->integer('id', true);
+			$table->string('name')->nullable();
+			$table->string('title')->nullable();
+			$table->string('text', 2048)->nullable();
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('articles');
+	}
+
+}
